c0b3a119033e6cebae12f8f558b27c35c53b1fda
ardome21 <ardome21@gmail.com>
2024-01-31 19:24:52 -0500

    first commit

---
+++ src/main/java/com/domingar/personalfinancesbackend/PersonalFinancesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domingar.personalfinancesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domingar/personalfinancesbackend/PersonalFinancesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingar.personalfinancesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
